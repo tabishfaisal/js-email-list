6d5362b6c1db48739f10880e51b27c26300332c8
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-11-05 16:25:17 +0100

    second commint

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("mail234");
+function generateEmail(){
+
+    const emailList = document.getElementById("emailList");
+    let emailitems = "";
+
+
+    function fetchEmail(){
+        return fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(res => res.json())
+        .then(data => {
+            const email = data.response;
+            emailitems += `<li>${email}</li>`;
+            emailList.innerHTML=emailitems; 
+        })
+        .catch(error => {
+            console.error('Errore', error);
+        });
+    }
+
+    for (let i= 0; i<10; i++){
+        fetchEmail();
+    }
+}
+
+generateEmail();
